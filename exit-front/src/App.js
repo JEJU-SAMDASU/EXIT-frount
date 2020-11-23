4d356ff63cb824013718548077806a3902771698
ohjunsang <pandati@dsm.hs.kr>
2020-11-23 21:42:23 +0900

    [FIX] File Struct

--- exit-front/src/App.js
+++ exit-front/src/App.js
@@ -1,5 +1,8 @@
+import React from "react";
+import Router from "./route";
+
 function App() {
-  return <div className="App"></div>;
+  return <Router />;
 }
 
 export default App;
---
+++ exit-front/src/route/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
