04d70ab7be3e59f85ab623942b11b50840bc94ed
강은빈 <kub0803@naver.com>
2020-11-23 23:48:02 +0900

    [Modify] 라우터 인덱스 수정

--- exit-front/src/route/index.js
+++ exit-front/src/route/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Signin, UserSignUp } from "../component/index";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route />
+        <Route path="/sign-in" component={Signin} exact />
+        <Route path="/user-signup" component={UserSignUp} exact />
       </Switch>
     </BrowserRouter>
   );
