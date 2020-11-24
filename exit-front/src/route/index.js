938b5c2e91dbe22207a9cb6513e9f5fe38183d45
강은빈 <kub0803@naver.com>
2020-11-24 10:52:38 +0900

    기본

--- exit-front/src/route/index.js
+++ exit-front/src/route/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Signin, UserSignUp } from "../component/index";
+import {
+  Signin,
+  UserSignUp,
+  MainPage,
+  ApplyView,
+  MyPage,
+} from "../component/index";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/" component={MainPage} exact />
         <Route path="/sign-in" component={Signin} exact />
         <Route path="/user-signup" component={UserSignUp} exact />
+        <Route path="/apply-view" component={ApplyView} exact />
+        <Route path="/mypage" component={MyPage} exact />
       </Switch>
     </BrowserRouter>
   );
